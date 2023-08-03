8f5e50a7522abfc44e60aef631a51c9e0445ce70
root <root@ferdinandimeka>
2023-08-03 23:29:55 +0100

    update

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Task 1 module
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+        concatenates two strings
+    """
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Task 2 module
+"""
+
+def floor(n: float) -> int:
+    """
+        returns floor of the float-point number
+    """
+    return int(n)
