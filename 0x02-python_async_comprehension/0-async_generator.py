ac2d73fb6d1c11c30bc4aa43f6ef1e874ba3358f
root <root@ferdinandimeka>
2023-08-09 16:20:35 +0100

    async_generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import asyncio
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator():
     """
         generates a sequence of ten numbers
     """
